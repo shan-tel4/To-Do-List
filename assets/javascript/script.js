9d6d58509f2204051959e964fe4f2f7c7d7b355d
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2024-11-26 19:13:14 +0000

    addTask function added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,14 +2,13 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-    console.log("addTask function triggered");
     if (inputBox.value === '') {
         alert("You must write something!");
-        console.log("Alert triggered for empty input");
+      
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        console.log("Task added:", inputBox.value);
+        
     }
 }
