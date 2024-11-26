48b4963bbd1f96fdeb69da5bc1ddc0a22b9fefa1
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2024-11-26 18:42:12 +0000

    js file type adjusted

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-
 function addTask() {
-    if(inputBox.value === ''){
+    console.log("addTask function triggered");
+    if (inputBox.value === '') {
         alert("You must write something!");
-    }
-    else{
+        console.log("Alert triggered for empty input");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        console.log("Task added:", inputBox.value);
     }
 }
